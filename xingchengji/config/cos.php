e6e1ba03bba356c96c91f9cffa08549d3eda7e0c
smashdog <smashdog@qq.com>
2019-01-17 19:17:04 +0800

    前后端及数据库分离

--- xingchengji/config/cos.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'COS_BUCKET' => '',
-    'COS_REGION' => '',
-    'COS_APPID' => '',
-    'COS_KEY' => '',
-    'COS_SECRET' => ''
-];
--- xingchengji_admin_html/src/views/admin/index/index2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        欢迎使用后台系统！
-    </div>
-</template>
